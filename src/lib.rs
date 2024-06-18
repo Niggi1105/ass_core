13fd77182bc76762c93bd070397416155797da47
Niggi1105 <niklas.liesch17@gmail.com>
2024-06-18 15:25:46 +0200

    added program struct 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use core::panic;
+use std::{
+    fs::File,
+    io::{BufRead, Read},
+};
+
 pub struct Instructionset {
     cpu: CPUSpecs,
     instructions: Vec<Box<dyn Instruction>>,
@@ -19,7 +25,56 @@ pub enum Token {
     Mnemonic(String),
     Number(i64),
     Register(String),
-    String(String),
+}
+
+pub struct Program {
+    lines: Vec<Vec<Token>>,
+}
+
+impl Program {
+    pub fn new(lines: Vec<Vec<Token>>) -> Self {
+        Self { lines }
+    }
+
+    pub fn from_file(path: String) -> Self {
+        let mut f = File::open(path).expect("can't open source file");
+        let mut source_text = String::new();
+        f.read_to_string(&mut source_text)
+            .expect("can't read source file");
+        let lines = source_text
+            .lines()
+            .map(|line| {
+                let raw_tokens: Vec<&str> = line.split_ascii_whitespace().collect();
+                let mut tokens = Vec::new();
+                if raw_tokens.len() == 0 {
+                    tokens
+                } else if raw_tokens[0][0..1] == *"//" {
+                    tokens
+                } else {
+                    tokens.push(Token::Mnemonic(raw_tokens[0].to_string()));
+                    tokens.append(&mut Self::tokenize(raw_tokens[1..].to_vec()));
+                    tokens
+                }
+            })
+            .filter(|line| line.len() >= 1)
+            .collect();
+        Self { lines }
+    }
+
+    fn tokenize(raw_tk: Vec<&str>) -> Vec<Token> {
+        raw_tk
+            .iter()
+            .map(|tk| {
+                if tk[0..0] == *"%" {
+                    Token::Register(tk[1..].to_string())
+                } else if let Ok(n) = tk.parse::<i64>() {
+                    Token::Number(n)
+                } else {
+                    panic!("unkown token")
+                }
+            })
+            .collect()
+    }
 }
 
 impl CPUSpecs {
@@ -49,4 +104,3 @@ impl Instructionset {
         }
     }
 }
-
